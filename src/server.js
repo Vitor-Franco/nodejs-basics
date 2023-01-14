79ad4c19e7ac3d55670c10f3f70a887ffc3f5969
Vitor Franco <vitoroliveirafranco@gmail.com>
2023-01-13 21:28:14 -0300

    feat: adiciona primeiro middleware

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 import http from 'node:http'
+import { json } from './middlewares/json.js';
 
 // CommonJS => utiliza require
 // ESModule => Novo padrão.. import/export.
@@ -12,21 +13,10 @@ const server = http.createServer(async (req, res) => {
 
   const { method, url } = req;
 
-  const buffers = []
-
-  // aguarda todos os chunks chegarem
-  for await (const chunk of req) {
-    buffers.push(chunk)
-  }
-
-  try {
-    req.body = JSON.parse(Buffer.concat(buffers).toString())
-  } catch {
-    req.body = null
-  }
+  await json(req, res);
 
   if(method === 'GET' && url === '/users') {
-    return res.setHeader('Content-type', 'application/json').end(JSON.stringify(users))
+    return res.end(JSON.stringify(users))
   }
 
   if(method === 'POST' && url === '/users') {
